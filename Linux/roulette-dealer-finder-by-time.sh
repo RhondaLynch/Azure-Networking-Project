d30ccc7e4121df5e3343165b93ec6618ddfe7706
Rhonda Lynch <84037324+RhondaLynch@users.noreply.github.com>
2021-09-24 17:03:09 -0700

    Delete roulette-dealer-finder-by-time.sh

--- Linux/roulette-dealer-finder-by-time.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Arg $1 = 4 char date ie 0310
-# Arg $2 = time+AM/PM which were concatenated in the previous step ie 05:00:00AM
-
-
-awk '{print $1 $2 "\t" $5 " " $6}' $1-dealer-schedule  | grep -i $2
